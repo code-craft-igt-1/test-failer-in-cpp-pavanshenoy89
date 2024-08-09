931ac2d98aca68d824c748a51c6228cfd3a1e4a9
pavanshenoy89 <71321360+pavanshenoy89@users.noreply.github.com>
2024-08-09 14:14:00 +0530

    Add files via upload

--- misaligned.cpp
+++ misaligned.cpp
@@ -1,5 +1,7 @@
 #include <assert.h>
 #include <iostream>
+#include <sstream>
+#include <string>
 
 int printColorMap() {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
@@ -13,9 +15,49 @@ int printColorMap() {
     return i * j;
 }
 
+void testPrintColorCodeMap() {
+    std::ostringstream oss;
+    std::streambuf* oldCoutBuffer = std::cout.rdbuf(oss.rdbuf());
+
+    PrintColorCodeMap();
+
+    std::cout.rdbuf(oldCoutBuffer);
+
+    std::string expectedOutput = 
+        "1 | White | Blue\n"
+        "2 | White | Orange\n"
+        "3 | White | Green\n"
+        "4 | White | Brown\n"
+        "5 | White | Slate\n"
+        "6 | Red | Blue\n"
+        "7 | Red | Orange\n"
+        "8 | Red | Green\n"
+        "9 | Red | Brown\n"
+        "10 | Red | Slate\n"
+        "11 | Black | Blue\n"
+        "12 | Black | Orange\n"
+        "13 | Black | Green\n"
+        "14 | Black | Brown\n"
+        "15 | Black | Slate\n"
+        "16 | Yellow | Blue\n"
+        "17 | Yellow | Orange\n"
+        "18 | Yellow | Green\n"
+        "19 | Yellow | Brown\n"
+        "20 | Yellow | Slate\n"
+        "21 | Violet | Blue\n"
+        "22 | Violet | Orange\n"
+        "23 | Violet | Green\n"
+        "24 | Violet | Brown\n"
+        "25 | Violet | Slate\n";
+
+    assert(oss.str() == expectedOutput);
+    std::cout << "All test cases passed!\n";
+}
+
 int main() {
     int result = printColorMap();
     assert(result == 25);
+    testPrintColorCodeMap();
     std::cout << "All is well (maybe!)\n";
     return 0;
 }
